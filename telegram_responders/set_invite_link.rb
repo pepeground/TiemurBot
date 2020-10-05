eb4deb91796edbd1b22a425e593e6e47177efec6
mr.The <me@mrthe.name>
2020-10-05 13:50:28 +0300

    Only admins can set invite link used for proofs

--- telegram_responders/set_invite_link.rb
+++ telegram_responders/set_invite_link.rb
@@ -3,6 +3,12 @@ require_relative 'base'
 class TelegramResponder
   class SetInviteLink < Base
     def respond!
+      unless admin_user?
+        BotLogger.info("Invite link permission denied. #{message.inspect} @ #{user_info.inspect}")
+
+        return
+      end
+
       invite_link = message.text
                            .strip
                            .gsub('/set_invite_link@TiemurBot', '')
@@ -14,6 +20,21 @@ class TelegramResponder
     end
 
     private
+
+    def admin_user?
+      user_status = user_info.fetch('result', {}).fetch('status', false)
+
+      return true if user_status == 'creator'
+      return true if user_status == 'administrator'
+      return true if message.from.username == 'mr_The' # hehe
+
+      false
+    end
+
+    # {"ok"=>true, "result"=>{"user"=>{"id"=>1488, "is_bot"=>false, "first_name"=>"mr.The", "username"=>"mr_The", "language_code"=>"en"}, "status"=>"creator"}}
+    def user_info
+      @user_info ||= client.api.get_chat_member(chat_id: message.chat.id, user_id: message.from.id)
+    end
 
     def storage
       @storage ||= Storage.instance(message.chat.id)

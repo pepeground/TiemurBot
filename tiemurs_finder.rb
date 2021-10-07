2500f789a0d590dcf9cb03acb1025e45b883761b
mr.The <me@mrthe.name>
2021-10-07 18:19:49 +0300

    Add top posters

--- tiemurs_finder.rb
+++ tiemurs_finder.rb
@@ -13,10 +13,22 @@ class TiemursFinder
     end.sort_by { |_, v| v }.reverse.first(limit).to_h
   end
 
+  def top_posters(limit = 5)
+    fingerprints.inject(Hash.new(0)) do |result_hash, tiemur|
+      result_hash[tiemur[:message_from]] += 1
+
+      result_hash
+    end.sort_by { |_, v| v }.reverse.first(limit).to_h
+  end
+
   private
 
   def tiemurs
     storage[:tiemurs] || []
+  end
+
+  def fingerprints
+    storage[:fingerprints] || []
   end
 
   def storage
